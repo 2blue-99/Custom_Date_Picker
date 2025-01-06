3d1226df36af479d1f2230c143332fb8dfeb937d
LeePureum <lpm083100@gmail.com>
2025-01-06 10:40:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/custom_date_picker/CustomDialog.kt
@@ -0,0 +1,96 @@
+package com.example.custom_date_picker
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.NumberPicker
+import androidx.fragment.app.DialogFragment
+import com.example.custom_date_picker.databinding.DialogDatePickerBinding
+
+class CustomDialog(): DialogFragment() {
+    private var _binding: DialogDatePickerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogDatePickerBinding.inflate(inflater, container, false)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDatePicker()
+    }
+
+    private fun initDatePicker(){
+        val currentDate = Calendar.getInstance()
+
+        val currentYear = currentDate.get(Calendar.YEAR)
+        val currentMonth = currentDate.get(Calendar.MONTH)
+        val currentDayOfMonth = currentDate.get(Calendar.DAY_OF_MONTH)
+
+        with(binding){
+            npYear.minValue = currentYear - 1
+            npYear.maxValue = currentYear + 1
+
+            npMonth.minValue = 1
+            npMonth.maxValue = 12
+
+            npDay.minValue = 1
+            npDay.maxValue = getDaysInMonth(currentYear, currentMonth)
+
+            npYear.displayedValues = getDisplayValues(currentYear-1, currentYear+1, "년")
+            npMonth.displayedValues = getDisplayValues(1, 12, "월")
+            npDay.displayedValues = getDisplayValues(1, 31, "일")
+
+            npYear.value = currentYear
+            npMonth.value = currentMonth
+            npDay.value = currentDate.get(Calendar.DAY_OF_MONTH)
+
+            // 수정 막기
+            npYear.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            npMonth.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            npDay.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+            npYear.setOnValueChangedListener { picker, oldVal, newVal ->
+                val maxDayValue = getDaysInMonth(newVal, npMonth.value)
+                npDay.maxValue = maxDayValue
+            }
+
+            npMonth.setOnValueChangedListener { picker, oldVal, newVal ->
+                val maxDayValue = getDaysInMonth(npYear.value, newVal)
+                npDay.maxValue = maxDayValue
+            }
+
+            confirmButton.setOnClickListener {
+                Log.e("TAG", "initDatePicker: ${npYear.value}. ${npMonth.value}. ${npDay.value}", )
+            }
+        }
+    }
+
+    private fun getDaysInMonth(year: Int, month: Int): Int {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month-1) // ?
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+    }
+
+    private fun getDisplayValues(start: Int, end: Int, suffix: String): Array<String> {
+        val displayValues = mutableListOf<String>()
+        for(value in start..end){
+            displayValues.add("${value}${suffix}")
+        }
+        return displayValues.toTypedArray()
+    }
+}
---
+++ app/src/main/java/com/example/custom_date_picker/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.custom_date_picker
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.custom_date_picker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.testBtn.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog(){
+        val dialog = CustomDialog()
+        dialog.show(this.supportFragmentManager, "custom")
+    }
+}
